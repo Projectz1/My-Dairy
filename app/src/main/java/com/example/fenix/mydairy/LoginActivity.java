d8f8364f1267fd11c33f84d0c495fadff7cbbf99
Joachim <joachim.wat@gmail.com>
2018-07-03 16:59:55 +0300

    Added required constraint on login

--- app/src/main/java/com/example/fenix/mydairy/LoginActivity.java
+++ app/src/main/java/com/example/fenix/mydairy/LoginActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.content.Intent;
+import android.widget.EditText;
 
 
 public class LoginActivity extends AppCompatActivity {
@@ -18,14 +19,54 @@ public class LoginActivity extends AppCompatActivity {
      * This method is executed when the login button is clicked
      */
     public void login(View v) {
-        Intent intent1 = new Intent(LoginActivity.this, Main2Activity.class);
-        startActivity(intent1);
+
+        EditText loginEmail = (EditText) findViewById(R.id.login_input_email);
+        EditText loginPassword = (EditText) findViewById(R.id.login_password);
+
+        /**
+         * This section ensures that the fields are filled before logging in
+         */
+        if (loginEmail.getText().toString().isEmpty()) {
+            loginEmail.setError("Email field is required");
+        } else if (loginPassword.getText().toString().isEmpty()) {
+            loginPassword.setError("Password field is required");
+        } else {
+            Intent intent1 = new Intent(LoginActivity.this, Main2Activity.class);
+            startActivity(intent1);
+        }
 
     }
 
-    public void signUp(View v) {
-        Intent intent2 = new Intent(LoginActivity.this, Main2Activity.class);
-        startActivity(intent2);
+    /**
+     * This method is executed when the create account button is clicked
+     */
+    public void signUp(View view) {
+        EditText firstName = (EditText) findViewById(R.id.first_name_input);
+        EditText lastName = (EditText) findViewById(R.id.last_name_input);
+        EditText signUpEmail = (EditText) findViewById(R.id.signup_input_email);
+        EditText sihnUpPhoneNumber = (EditText) findViewById(R.id.signup_phone);
+        EditText signUpPassword = (EditText) findViewById(R.id.signup_password);
+        EditText confirmPassword = (EditText) findViewById(R.id.confirm_password);
+
+        /**
+         * This section ensures that the fields are filled before logging in
+         */
+        if (firstName.getText().toString().isEmpty()) {
+            firstName.setError("First name is required");
+        } else if (lastName.getText().toString().isEmpty()) {
+            lastName.setError("Last name is required");
+        } else if (signUpEmail.getText().toString().isEmpty()) {
+            signUpEmail.setError("Your email is required");
+        } else if (sihnUpPhoneNumber.getText().toString().isEmpty()) {
+            sihnUpPhoneNumber.setError("Phone number is required");
+        } else if (signUpPassword.getText().toString().isEmpty()) {
+            signUpPassword.setError("Password is required");
+        } else if (confirmPassword.getText().toString().isEmpty()) {
+            confirmPassword.setError(" Confirm Password is required");
+        } else {
+            Intent intent2 = new Intent(LoginActivity.this, Main2Activity.class);
+            startActivity(intent2);
+        }
 
     }
 }
