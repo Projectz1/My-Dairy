15461812c0dc8256da85d4b54f1b8b88e60c0610
Joachim <joachim.wat@gmail.com>
2018-07-04 18:36:47 +0300

    Added back buttons on suppliers and Add new suppliers activities, without functionality

--- app/src/main/java/com/example/fenix/mydairy/EachSupplierActivity.java
+++ app/src/main/java/com/example/fenix/mydairy/EachSupplierActivity.java
@@ -8,6 +8,6 @@ public class EachSupplierActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_each_supplier);
+
     }
 }
